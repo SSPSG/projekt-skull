3f3c098c26c57d18cb277ec77ab78f8edcb06fca
Marek Vykysalý <mara.vyky@gmail.com>
2023-04-06 21:31:39 +0200

    feat: add style for auth component

--- src/lib/auth.svelte
+++ src/lib/auth.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
     import { pb, currentUser } from '$lib/pocketbase';
+    import '/src/scss/authentification.scss';
+
+    export let register: boolean = false;
 
     let username: string;
+    let email: string;
     let password: string;
     let passwordConfirm: string;
 
-    let signUpUI: boolean = false;
     let loginError: boolean = false;
+    let registerEmailError: boolean = false;
 
     async function login() {
         try {
             loginError = false;
             const user = await pb.collection('users').authWithPassword(username, password);
         }
-        catch (error) {
+        catch (error: any) {
+            console.log(error.data);
             loginError = true;
         }
     }
@@ -22,6 +27,7 @@
         if (password != passwordConfirm) return;
         const data = {
             username,
+            email,
             password,
             passwordConfirm
         }
@@ -38,20 +44,27 @@
     <p>{$currentUser.username}</p>
     <button on:click={logOut}>Log out</button>
 {:else}
-    Sign Up <input type="checkbox" bind:checked={signUpUI}>
-        {#if signUpUI}
-        <input type="text" placeholder="username or email" bind:value={username}>
-        <input type="password" placeholder="password" bind:value={password}>
-        <input type="password" placeholder="confirm password" bind:value={passwordConfirm}>
-        <button on:click={signUp}>Submit</button>
+    {#if register}
+        <div class="signup">
+            <h1>Sign in</h1>
+            <form>
+                <input type="text" placeholder="username or email" required bind:value={username}>
+                <input type="email" placeholder="email" required bind:value={email}>
+                <input type="password" placeholder="password" required bind:value={password}>
+                <input type="password" placeholder="confirm password" required bind:value={passwordConfirm}>
+                <button type="submit" on:click={signUp}><p>Create account</p></button>
+            </form>
+        </div> 
     {:else}
-        {#if loginError}
-        <p style="color=red;">Wrong username or password!</p>
-        {/if}
-        <form>
-            <input type="text" placeholder="username or email" bind:value={username}>
-            <input type="password" placeholder="password" bind:value={password}>
-            <button on:click={login}>Submit</button>
-        </form>
+        <div class="signin">
+            <h1>Sign in</h1>
+            <form>
+                <input type="text" placeholder="username or email" required bind:value={username}>
+                <input type="password" placeholder="password" required bind:value={password}>
+                <a href="#top">Forgor💀 password?</a>
+                <button type="submit" on:click={login}><p>Sign in</p></button>
+            </form>
+            <p class="no-account">Don't have an account? <a href="#top">Sign Up</a>!</p>
+        </div> 
     {/if}
 {/if}
