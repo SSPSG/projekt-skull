6ba7d3d4cc3dbf331d4bc51dc11d694f9f6ae70b
AAA1exa8 <84516210+AAA1exa8@users.noreply.github.com>
2023-04-08 00:11:06 +0200

    fix bad css

--- src/lib/auth.svelte
+++ src/lib/auth.svelte
@@ -1,126 +1,167 @@
 <script lang="ts">
+    
+	/*
+	 * Junk code!
+	 */
 
-    /*
-     * Junk code!
-     */
+	import { pb, currentUser } from '$lib/pocketbase';
+	import { regEx, validate } from '$lib/validation';
+	import '/src/scss/authentification.scss';
 
-    import { pb, currentUser } from '$lib/pocketbase';
-    import { regEx, validate } from '$lib/validation';
-    import '/src/scss/authentification.scss';
+	export let register: boolean = false;
 
-    export let register: boolean = false;
+	let usernameInput: HTMLInputElement;
+	let emailInput: HTMLInputElement;
+	let passwordInput: HTMLInputElement;
+	let passwordConfirm: string;
 
-    let usernameInput: HTMLInputElement;
-    let emailInput: HTMLInputElement;
-    let passwordInput: HTMLInputElement;
-    let passwordConfirm: string;
+	let authErrorText: string = '';
+	let authError: boolean = false;
 
-    let authErrorText: string = "";
-    let authError: boolean = false;
+	async function sendRequest() {
+		const data = {
+			username: usernameInput.value,
+			email: emailInput.value,
+			password: passwordInput.value,
+			passwordConfirm: passwordConfirm
+		};
+		await pb.collection('users').create(data);
+	}
 
-    async function sendRequest()
-    {
-        const data = {
-            username: usernameInput.value,
-            email: emailInput.value,
-            password: passwordInput.value,
-            passwordConfirm: passwordConfirm
-        }
-        await pb.collection('users').create(data);
-    }
+	function checkForInvalidInput() {
+		const inputs: HTMLInputElement[] = [usernameInput, emailInput, passwordInput];
+		for (let i = 0; i < inputs.length; i++) {
+			let currentInput: HTMLInputElement = inputs[i];
+			if (currentInput == undefined) continue;
+			let inputName: string = currentInput.name;
+			if (!validate(currentInput, regEx[inputName])) return false;
+		}
+		return true;
+	}
 
-    function checkForInvalidInput() {
-        const inputs: HTMLInputElement[] = [usernameInput, emailInput, passwordInput];
-        for (let i = 0; i < inputs.length; i++) {
-            let currentInput: HTMLInputElement = inputs[i];
-            if (currentInput == undefined) continue;
-            let inputName: string = currentInput.name;
-            if (!validate(currentInput, regEx[inputName])) return false;
-        }
-        return true;
-    }
+	function onValidate(input: HTMLInputElement, regEx: RegExp) {
+		if (!validate(input, regEx)) input.className = 'invalid';
+		else input.className = 'valid';
+	}
 
-    function onValidate(input: HTMLInputElement, regEx: RegExp) {
-        if (!validate(input, regEx)) input.className = 'invalid';
-        else input.className = 'valid';
-    }
+	function authentificationError(text: string, throwError: boolean): void {
+		authError = throwError;
+		authErrorText = text;
+	}
 
-    function authentificationError(text: string, throwError: boolean): void {
-        authError = throwError;
-        authErrorText = text;
-    }
+	async function login() {
+		authentificationError('', false);
+		try {
+			if (!checkForInvalidInput()) return;
+			await pb.collection('users').authWithPassword(emailInput.value, passwordInput.value);
+		} catch (error) {
+			authentificationError('Invalid username or password!', true);
+		}
+	}
 
-    async function login() {
-        authentificationError("", false);
-        try {
-            if (!checkForInvalidInput()) return;
-            await pb.collection('users').authWithPassword(emailInput.value, passwordInput.value);
-        } catch (error) {
-            authentificationError("Invalid username or password!", true);
-        }
-    }
+	async function signUp() {
+		authentificationError('', false);
+		try {
+			if (passwordInput.value != passwordConfirm) {
+				authentificationError("Passwords don't match!", true);
+				return;
+			} else if (!checkForInvalidInput()) return;
 
-    async function signUp() {
-        authentificationError("", false);
-        try {
-            if (passwordInput.value != passwordConfirm) {
-                authentificationError("Passwords don't match!", true);
-                return;
-            } else if (!checkForInvalidInput()) return;
+			await sendRequest();
 
-            await sendRequest();
+			login();
+		} catch (error) {
+			authentificationError('Unexpected error or email is already used!', true);
+		}
+	}
 
-            login();
-        } catch (error)
-        {
-            authentificationError("Unexpected error or email is already used!", true);
-        }
-    }
-
-    function logOut() {
-        pb.authStore.clear();
-    }
+	function logOut() {
+		pb.authStore.clear();
+	}
 </script>
 
 {#if $currentUser}
-    <p>{$currentUser.username}</p>
-    <button on:click={logOut}>Log out</button>
+	<div class="body">
+		<p>{$currentUser.username}</p>
+		<button on:click={logOut}>Log out</button>
+	</div>
+{:else if register}
+	<div class="body">
+		<div class="signup">
+			<h1>Sign up</h1>
+			<form>
+				{#if authError}
+					<p class="error-text">{authErrorText}</p>
+				{/if}
+				<input
+					type="text"
+					name="password"
+					placeholder="username"
+					required
+					on:keyup={() => onValidate(usernameInput, regEx.username)}
+					bind:this={usernameInput}
+				/>
+				<p>Invalid username!</p>
+				<input
+					type="email"
+					name="email"
+					placeholder="email"
+					required
+					on:keyup={() => onValidate(emailInput, regEx.email)}
+					bind:this={emailInput}
+				/>
+				<p>Invalid email!</p>
+
+				<input
+					type="password"
+					name="password"
+					placeholder="password"
+					on:keyup={() => onValidate(passwordInput, regEx.password)}
+					required
+					bind:this={passwordInput}
+				/>
+
+				<p>Invalid password!</p>
+				<input
+					type="password"
+					placeholder="confirm password"
+					required
+					bind:value={passwordConfirm}
+				/>
+				<p>passwords don't match!</p>
+				<button on:click={signUp}><p>Create account</p></button>
+			</form>
+		</div>
+	</div>
 {:else}
-    {#if register}
-        <div class="signup">
-            <h1>Sign up</h1>
-            <form >
-                {#if authError}
-                    <p class="error-text">{authErrorText}</p>
-                {/if}
-                <input type="text" name="password" placeholder="username" required on:keyup={() => onValidate(usernameInput, regEx.username)} bind:this={usernameInput}>
-                <p>Invalid username!</p>
-                <input type="email" name="email" placeholder="email" required on:keyup={() => onValidate(emailInput, regEx.email)} bind:this={emailInput}>
-                <p>Invalid email!</p>
-                
-                <input type="password" name="password" placeholder="password" on:keyup={() => onValidate(passwordInput, regEx.password)} required bind:this={passwordInput}>
-                
-                <p>Invalid password!</p>
-                <input type="password" placeholder="confirm password" required bind:value={passwordConfirm}>
-                <p>passwords don't match!</p>
-                <button on:click={signUp}><p>Create account</p></button>
-            </form>
-        </div> 
-    {:else}
-        <div class="signin">
-            <h1>Sign in</h1>
-            <form on:submit|preventDefault={login}>
-                {#if authError}
-                    <p class="error-text">{authErrorText}</p>
-                {/if}
-                <input type="text" name="email" placeholder="email" required on:keyup={() => onValidate(emailInput, regEx.email)} bind:this={emailInput}>
-                <p>Invalid email!</p>
-                
-                <input type="password" name="password" placeholder="password" required bind:this={passwordInput}>
-                <a href="#top">Forgor💀 password?</a>
-                <button type="submit"><p>Sign in</p></button>
-            </form>
-            <p class="no-account">Don't have an account? <a href="#top">Sign Up</a>!</p>
-        </div> 
-    {/if}
+	<div class="body">
+		<div class="signin">
+			<h1>Sign in</h1>
+			<form on:submit|preventDefault={login}>
+				{#if authError}
+					<p class="error-text">{authErrorText}</p>
+				{/if}
+				<input
+					type="text"
+					name="email"
+					placeholder="email"
+					required
+					on:keyup={() => onValidate(emailInput, regEx.email)}
+					bind:this={emailInput}
+				/>
+				<p>Invalid email!</p>
+
+				<input
+					type="password"
+					name="password"
+					placeholder="password"
+					required
+					bind:this={passwordInput}
+				/>
+				<a href="#top">Forgor💀 password?</a>
+				<button type="submit"><p>Sign in</p></button>
+			</form>
+			<p class="no-account">Don't have an account? <a href="#top">Sign Up</a>!</p>
+		</div>
+	</div>
 {/if}
