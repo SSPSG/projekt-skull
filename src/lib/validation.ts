1dda815f2232db6c753c98f5cd378e25e5d90008
AAA1exa8 <84516210+AAA1exa8@users.noreply.github.com>
2023-04-08 00:34:15 +0200

    change regex to accept longer emails

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -2,7 +2,7 @@ export const regEx: any = {
     // Password can contain any alphanumerical characters, '+', '_', '-', '.' and its length must be 8 up to 64
     username: /^[\w\-\+\.]{3,20}$/,
     // Email RegEx format: example.exa-_mple+example@example.co.uk
-    email: /^([a-z\d_\-\.\+]+)@([a-z\d_\-]{2,8})\.([a-z\d]{2,8})(\.[a-z\d]{2,8})?$/,
+    email: /^([a-z\d_\-\.\+]+)@([a-z\d_\-]{2,15})\.([a-z\d]{2,15})(\.[a-z\d]{2,8})?$/,
     // Password has the same RegEx as username
     password: /^([\w\-\+\.]{8,64})$/,
 };
